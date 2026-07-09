e4f97dc7a6237a71300b9c1e21099fea94b9a7f2
Oxala272 <havennancy45@gmail.com>
2026-07-09 15:17:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -33,4 +33,10 @@ startButton.onclick = async function () {
         console.log(e);
     }
 };
+<script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js"></script>
 
+<script src="app.js"></script>
+
+</body>
+</html>
