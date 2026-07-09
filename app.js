74a445d9ff217c37824ceba9100f5f67bab07c6c
Oxala272 <havennancy45@gmail.com>
2026-07-09 16:17:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,43 @@ button.onclick = async () => {
 
     video.srcObject = stream;
     await video.play();
+const faceMesh = new FaceMesh({
 
+  locateFile: (file) =>
+
+    `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
+
+});
+
+faceMesh.setOptions({
+
+  maxNumFaces: 1,
+
+  refineLandmarks: true,
+
+  minDetectionConfidence: 0.5,
+
+  minTrackingConfidence: 0.5
+
+});
+
+faceMesh.onResults(onResults);
+
+const camera = new Camera(video, {
+
+  onFrame: async () => {
+
+    await faceMesh.send({ image: video });
+
+  },
+
+  width: 640,
+
+  height: 480
+
+});
+
+camera.start();
     video.onloadedmetadata = () => {
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
@@ -36,3 +72,30 @@ button.onclick = async () => {
     console.error(error);
   }
 };
+function onResults(results) {
+  const canvas = document.getElementById("output");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = results.image.width;
+  canvas.height = results.image.height;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(results.image, 0, 0);
+
+  if (results.multiFaceLandmarks) {
+    for (const landmarks of results.multiFaceLandmarks) {
+      for (const point of landmarks) {
+        ctx.beginPath();
+        ctx.arc(
+          point.x * canvas.width,
+          point.y * canvas.height,
+          1.5,
+          0,
+          Math.PI * 2
+        );
+        ctx.fillStyle = "#00ff00";
+        ctx.fill();
+      }
+    }
+  }
+}
